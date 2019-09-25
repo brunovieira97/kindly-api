eed2693957242b7b8ce995c4372171fc1cc69649
Bruno Vieira <bruno.vieira97@outlook.com>
2019-09-25 01:23:27 -0300

    Cleared main class (App) to allow easier API tests

--- backend/src/main/java/app/App.java
+++ backend/src/main/java/app/App.java
@@ -1,11 +1,11 @@
 package app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import dao.*;
+import model.*;
+
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }

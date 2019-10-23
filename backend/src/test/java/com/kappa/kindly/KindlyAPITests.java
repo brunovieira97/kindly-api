fb55d23ecb6690710050fa77c932bad39fe529ea
Bruno Vieira <bruno.vieira97@outlook.com>
2019-10-22 23:45:59 -0300

    Fixed main test class path

--- backend/src/test/java/com/kappa/kindly/KindlyAPITests.java
+++ backend/src/test/java/com/kappa/kindly/KindlyAPITests.java
@@ -1,10 +1,10 @@
-package com.kappa.kindly.api;
+package com.kappa.kindly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class KindlyAPITests {
 
 	@Test
 	void contextLoads() {

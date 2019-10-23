5612d90ba984b022a0883ddeb31ff0afa5aa2122
Bruno Vieira <bruno.vieira97@outlook.com>
2019-10-23 20:54:35 -0300

    Criados endpoints de Necessidades (#42)
    * Adicionados endpoints para cadastro de necessidade
    
    * Adicionada quebra de linha ao fim das classes

---
+++ backend/src/main/java/com/kappa/kindly/controller/WishlistController.java
@@ -0,0 +1,67 @@
+package com.kappa.kindly.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.kappa.kindly.model.Wishlist;
+import com.kappa.kindly.repository.WishlistRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/wishlist")
+public class WishlistController {
+
+	@Autowired
+	private WishlistRepository wishlistRepository;
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Wishlist> FindByID(@Valid @PathVariable(value = "id") long id) {
+
+		if ((!wishlistRepository.existsById(id)) || (wishlistRepository.findById(id) == null))
+			return ResponseEntity.notFound().build();
+		
+		return ResponseEntity.ok(wishlistRepository.findById(id));
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Wishlist>> FindAll() {
+		
+		return ResponseEntity.ok(wishlistRepository.findAll());
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Wishlist> Create(@Valid @RequestBody Wishlist Wishlist) {
+		
+		return ResponseEntity.ok(wishlistRepository.save(Wishlist));
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public ResponseEntity<Wishlist> Update(@Valid @RequestBody Wishlist Wishlist) {
+		
+		if (!wishlistRepository.existsById(Wishlist.getId()))
+			return ResponseEntity.notFound().build();
+
+		return ResponseEntity.ok(wishlistRepository.save(Wishlist));
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Wishlist> Delete(@Valid @PathVariable(value = "id") long id) {
+
+		if (!wishlistRepository.existsById(id))
+			return ResponseEntity.notFound().build();
+
+		Wishlist WishlistResponse = wishlistRepository.findById(id);
+		wishlistRepository.deleteById(id);
+
+		return ResponseEntity.ok(WishlistResponse);
+	}
+}

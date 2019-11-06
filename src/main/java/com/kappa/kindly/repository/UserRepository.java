dd462ab03c6166e13fbeec6f4483f5131e878489
Bruno Vieira <bruno.vieira97@outlook.com>
2019-11-06 04:59:38 -0300

    Preparadas classes existentes para autenticação
    * Removidos endpoints de autenticação de UserController
    * Atualizada classe User para implementar UserDetails
    * Criado método para obter User por e-mail

--- src/main/java/com/kappa/kindly/repository/UserRepository.java
+++ src/main/java/com/kappa/kindly/repository/UserRepository.java
@@ -1,13 +1,19 @@
 package com.kappa.kindly.repository;
 
+import java.util.Optional;
+
 import com.kappa.kindly.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	User findById(long id);
+
+	@Query("Select U from User U where U.email = ?1")
+	Optional<User> findByEmail(String email);
 	
 }

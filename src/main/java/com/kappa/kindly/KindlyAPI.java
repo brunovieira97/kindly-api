e1164a1521e42f2d1b5f4787750113c6df726a72
Bruno Vieira <bruno.vieira97@outlook.com>
2019-11-10 00:58:48 -0300

    Tentativa de correção para o problema do CORS
    Quando utilizadas múltiplas classes de configuração do Spring Boot,
    ocorria erro pois provavelmente apenas a primeira classe é carregada.
    
    Como foi criada uma para o JWT e existia uma para o CORS, foi necessário
    combiná-las. Fiz a classe WebConfig importar a WebSecurityConfig, para
    testar.

--- src/main/java/com/kappa/kindly/KindlyAPI.java
+++ src/main/java/com/kappa/kindly/KindlyAPI.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.kappa.kindly.model"})
-@EnableJpaRepositories(basePackages = {"com.kappa.kindly.repository"})
+@EntityScan
+@EnableJpaRepositories
 public class KindlyAPI {
 
 	public static void main(String[] args) {
--- src/main/java/com/kappa/kindly/config/WebConfig.java
+++ src/main/java/com/kappa/kindly/config/WebConfig.java
@@ -1,12 +1,14 @@
 package com.kappa.kindly.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Import(WebSecurityConfig.class)
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override

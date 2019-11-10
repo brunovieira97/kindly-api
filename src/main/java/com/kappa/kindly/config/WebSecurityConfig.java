1340a145765afb9edda4f0153236fee5acd217b6
Bruno Vieira <bruno.vieira97@outlook.com>
2019-11-10 10:41:39 -0300

    Adicionado Bean para os filtros do CORS
    Criada classe CorsConfig, que substitui WebConfig. A mesma retorna um
    Bean corsFilter, que é utilizado automaticamente em WebSecurityConfig.
    
    Em WebSecurityConfig, foi adicionado tratamento para o CORS.

--- src/main/java/com/kappa/kindly/config/WebSecurityConfig.java
+++ src/main/java/com/kappa/kindly/config/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
 			.csrf().disable()
+			.cors().and()
 			.authorizeRequests()
 				.antMatchers("/signin", "/signup").permitAll()
 				.antMatchers(HttpMethod.GET, "/institution", "/institution/**").permitAll()
